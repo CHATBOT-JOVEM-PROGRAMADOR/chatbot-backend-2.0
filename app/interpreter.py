fedd458a7a62bce2db02cc508019ec2dabf44f7c
Alan Mengarda <134882877+Alanmengarda@users.noreply.github.com>
2025-11-24 14:20:44 -0300

    Delete app directory

--- app/interpreter.py
+++
@@ -1,8 +0,0 @@
-def buscar_resposta(pergunta, texto):
-    palavras_chave = pergunta.lower().split()
-    linhas = texto.split('\n')
-    
-    for linha in linhas:
-        if all(p in linha.lower() for p in palavras_chave):
-            return linha
-    return None
